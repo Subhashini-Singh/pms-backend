e74d3edf75c08b30367c7d80fb3101cba40e0cda
subhashini <ssingh@argusoft.com>
2023-07-18 17:34:12 +0530

    Login and sign up

--- src/main/java/com/project/tracker/entity/User.java
+++ src/main/java/com/project/tracker/entity/User.java
@@ -5,7 +5,9 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.NaturalId;
 
 import javax.management.relation.Role;
@@ -14,38 +16,30 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
-@Table(name = "users", uniqueConstraints = {
-        @UniqueConstraint(columnNames = {
-                "username"
-        }),
-        @UniqueConstraint(columnNames = {
-                "email"
-        })
-})
+@Table(name = "users")
 
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @NotBlank
-    @Size(min=3, max = 50)
-    private String name;
 
     @NotBlank
-    @Size(min=3, max = 50)
     private String username;
 
     @NaturalId
     @NotBlank
-    @Size(max = 50)
     @Email
     private String email;
 
     @NotBlank
-    @Size(min=6, max = 100)
     private String password;
+
+    private String role;
+    private String status;
 
     //@ManyToMany(fetch = FetchType.LAZY)
     //@JoinTable(name = "user_roles",
